93827d527fc200f3f781523509fade097ec7b321
Mathieu <mathieu.belanger.2@ens.etsmtl.ca>
2015-11-17 12:17:26 -0500

    B-spline semi-fonctionnel

--- src/model/BSplineCurveType.java
+++ src/model/BSplineCurveType.java
@@ -1,9 +1,16 @@
 package model;
 
-import java.awt.Point;
+import java.awt.*;
 import java.util.List;
 
 public class BSplineCurveType extends CurveType {
+
+    private List bSplineMatrix =
+            Matrix.buildMatrix4(-1 * 1.0 / 6.0, 3 * 1.0 / 6.0, -3 * 1.0 / 6.0, 1 * 1.0 / 6.0,
+                    3 * 1.0 / 6.0, -6 * 1.0 / 6.0, 3 * 1.0 / 6.0, 0 * 1.0 / 6.0,
+                    -3 * 1.0 / 6.0, 3 * 1.0 / 6.0, 0 * 1.0 / 6.0, 0 * 1.0 / 6.0,
+                    1 * 1.0 / 6.0, 0 * 1.0 / 6.0, 0 * 1.0 / 6.0, 0 * 1.0 / 6.0);
+    private List matrix = bSplineMatrix;
 
 	public BSplineCurveType(String name) {
 		super(name);
@@ -13,8 +20,8 @@ public class BSplineCurveType extends CurveType {
 	@Override
 	public int getNumberOfSegments(int numberOfControlPoints) {
 		if (numberOfControlPoints >= 4) {
-			return numberOfControlPoints-3;
-		} else {
+            return numberOfControlPoints - 3;
+        } else {
 			return 0;
 		}
 	}
@@ -27,30 +34,22 @@ public class BSplineCurveType extends CurveType {
 
 	@Override
 	public ControlPoint getControlPoint(List controlPoints, int segmentNumber, int controlPointNumber) {
-		System.out.println(controlPoints.size());
-		System.out.println(segmentNumber);
-		System.out.println(controlPointNumber);
 		int controlPointIndex = segmentNumber + controlPointNumber;
 		return (ControlPoint)controlPoints.get(controlPointIndex);
 	}
 
-	@Override
+    @Override
 	public Point evalCurveAt(List controlPoints, double t) {
 		List tVector = Matrix.buildRowVector4(t*t*t, t*t, t, 1);
-		List gVector = Matrix.buildColumnVector4(((ControlPoint)controlPoints.get(0)).getCenter(), 
-			((ControlPoint)controlPoints.get(1)).getCenter(), 
-			((ControlPoint)controlPoints.get(2)).getCenter(),
-			((ControlPoint)controlPoints.get(3)).getCenter());
-		Point p = Matrix.eval(tVector, matrix, gVector);
+
+        Point pi3 = ((ControlPoint) controlPoints.get(0)).getCenter();
+        Point pi2 = ((ControlPoint) controlPoints.get(1)).getCenter();
+        Point pi1 = ((ControlPoint) controlPoints.get(2)).getCenter();
+        Point pi = ((ControlPoint) controlPoints.get(3)).getCenter();
+
+        List gVector = Matrix.buildColumnVector4(pi, pi1, pi2, pi3);
+        Point p = Matrix.eval(tVector, matrix, gVector);
 		return p;
 	}
-
-	private List bezierMatrix = 
-		Matrix.buildMatrix4(-1*1.0/6.0,  3*1.0/6.0, -3*1.0/6.0, 1*1.0/6.0, 
-							 3*1.0/6.0, -6*1.0/6.0,  3*1.0/6.0, 0*1.0/6.0, 
-							-3*1.0/6.0,  3*1.0/6.0,  0*1.0/6.0, 0*1.0/6.0, 
-							 1*1.0/6.0,  0*1.0/6.0,  0*1.0/6.0, 0*1.0/6.0);
-							 
-	private List matrix = bezierMatrix;
 
 }
