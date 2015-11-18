c0ac894e72dc0a985492aa901f7969993990a4f7
Mathieu <mathieu.belanger.2@ens.etsmtl.ca>
2015-11-18 14:54:18 -0500

    Removing useless printout allo

--- src/model/BezierCurveType.java
+++ src/model/BezierCurveType.java
@@ -28,56 +28,56 @@ import java.util.List;
  */
 public class BezierCurveType extends CurveType {
 
-	private List bezierMatrix =
-		Matrix.buildMatrix4(-1,  3, -3, 1,
-							 3, -6,  3, 0,
-							-3,  3,  0, 0,
-							 1,  0,  0, 0);
-	private List matrix = bezierMatrix;
+    private List bezierMatrix =
+            Matrix.buildMatrix4(-1, 3, -3, 1,
+                    3, -6, 3, 0,
+                    -3, 3, 0, 0,
+                    1, 0, 0, 0);
+    private List matrix = bezierMatrix;
 
 	public BezierCurveType(String name) {
 		super(name);
 	}
 
 	/* (non-Javadoc)
-	 * @see model.CurveType#getNumberOfSegments(int)
+     * @see model.CurveType#getNumberOfSegments(int)
 	 */
-	public int getNumberOfSegments(int numberOfControlPoints) {
-		if (numberOfControlPoints >= 4) {
-			return (numberOfControlPoints - 1) / 3;
-		} else {
-			return 0;
-		}
-	}
+    public int getNumberOfSegments(int numberOfControlPoints) {
+        if (numberOfControlPoints >= 4) {
+            return (numberOfControlPoints - 1) / 3;
+        } else {
+            return 0;
+        }
+    }
 
 	/* (non-Javadoc)
-	 * @see model.CurveType#getNumberOfControlPointsPerSegment()
+     * @see model.CurveType#getNumberOfControlPointsPerSegment()
 	 */
-	public int getNumberOfControlPointsPerSegment() {
-		return 4;
-	}
+    public int getNumberOfControlPointsPerSegment() {
+        return 4;
+    }
 
 	/* (non-Javadoc)
-	 * @see model.CurveType#getControlPoint(java.util.List, int, int)
+     * @see model.CurveType#getControlPoint(java.util.List, int, int)
 	 */
-	public ControlPoint getControlPoint(
-		List controlPoints,
-		int segmentNumber,
-		int controlPointNumber) {
-		int controlPointIndex = segmentNumber * 3 + controlPointNumber;
-		return (ControlPoint)controlPoints.get(controlPointIndex);
-	}
-							 
-	/* (non-Javadoc)
-	 * @see model.CurveType#evalCurveAt(java.util.List, double)
-	 */
-	public Point evalCurveAt(List controlPoints, double t) {
-		List tVector = Matrix.buildRowVector4(t*t*t, t*t, t, 1);
+    public ControlPoint getControlPoint(
+            List controlPoints,
+            int segmentNumber,
+            int controlPointNumber) {
+        int controlPointIndex = segmentNumber * 3 + controlPointNumber;
+        return (ControlPoint) controlPoints.get(controlPointIndex);
+    }
 
-		List gVector = Matrix.buildColumnVector4(((ControlPoint)controlPoints.get(0)).getCenter(),
-			((ControlPoint)controlPoints.get(1)).getCenter(),
-			((ControlPoint)controlPoints.get(2)).getCenter(),
-			((ControlPoint)controlPoints.get(3)).getCenter());
+    /* (non-Javadoc)
+     * @see model.CurveType#evalCurveAt(java.util.List, double)
+     */
+    public Point evalCurveAt(List controlPoints, double t) {
+        List tVector = Matrix.buildRowVector4(t*t*t, t*t, t, 1);
+
+        List gVector = Matrix.buildColumnVector4(((ControlPoint) controlPoints.get(0)).getCenter(),
+                ((ControlPoint) controlPoints.get(1)).getCenter(),
+                ((ControlPoint) controlPoints.get(2)).getCenter(),
+                ((ControlPoint)controlPoints.get(3)).getCenter());
 		Point p = Matrix.eval(tVector, matrix, gVector);
 		return p;
 	}
